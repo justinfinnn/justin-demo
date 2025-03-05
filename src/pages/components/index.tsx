053846f8820fdbbc376ea06a98b4ac3c8e148f61
justinfinnn <justinfinnegan18@gmail.com>
2025-03-04 19:09:08 -0500

    unused import

--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -3,7 +3,6 @@ import { TwoColumn } from "@/components/TwoColumn/TwoColumn";
 import { SectionContainer } from "@/components/SectionContainer/SectionContainer";
 import { Subheading } from "@/components/Subheading/Subheading";
 import Image from "next/image";
-import Link from "next/link";
 import { CTA } from "@/components/CTA/CTA";
 
 export default function Components() {
